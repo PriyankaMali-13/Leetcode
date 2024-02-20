b2b956fb800d85617087e5cde21778e390961be0
Priyanka Mali <priyankamali0000@gmail.com>
2024-02-20 07:03:10 +0000

    Sync LeetCode submission - Missing Number (python)

--- problems/missing_number/solution.py
+++ problems/missing_number/solution.py
@@ -1,5 +1,9 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         for i in range(len(nums)+1):
             if i not in nums:
                 return i
