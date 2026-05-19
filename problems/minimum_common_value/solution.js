b2b1d7edbd1eab3ece39f0bdd0d9ec9691549ca0
Priyanka Mali <priyankamali0000@gmail.com>
2026-05-19 14:18:40 +0000

    Sync LeetCode submission - Minimum Common Value (javascript)

---
+++ problems/minimum_common_value/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function (nums1, nums2) {
+    let ptr1 = 0;
+    let ptr2 = 0;
+
+    while (ptr1 < nums1.length && ptr2 < nums2.length) {
+        if(nums1[ptr1] === nums2[ptr2]){
+            return nums1[ptr1];
+        }
+        else if(nums1[ptr1] < nums2[ptr2]){
+            ptr1++;
+        }
+        else{
+            ptr2++;
+        }
+    }
+    return -1;
+};
