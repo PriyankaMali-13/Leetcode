2f81e437392bc184ae7eb89b575e462400e0c019
Priyanka Mali <priyankamali0000@gmail.com>
2026-06-08 19:30:06 +0000

    Sync LeetCode submission - Sqrt(x) (javascript)

--- problems/sqrt(x)/solution.js
+++ problems/sqrt(x)/solution.js
@@ -5,8 +5,32 @@
 var mySqrt = function(x) {
     if(x === 0) return 0;
     let i = 1;
-    while(i*i <=x){
+    while(i*i <= x){
         i++;
     }
     return i-1;
 };
+
+/*
+Binary Search approach
+var mySqrt = function(x) {
+    if(x === 0) return 0;
+    let left = 1;
+    let right = x;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        
+        if(mid * mid === x){
+            return mid;
+        }
+        else if(mid * mid < x){
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return right;
+};
+*/
