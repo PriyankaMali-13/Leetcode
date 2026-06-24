26c27bd42c86e008dc335d3a5f981e5eb13269c0
Priyanka Mali <priyankamali0000@gmail.com>
2026-06-24 14:09:15 +0000

    Sync LeetCode submission - Container With Most Water (javascript)

---
+++ problems/container_with_most_water/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length -1;
+    while(left < right){
+        let area = (right - left) * Math.min(height[left], height[right]);
+        maxArea = Math.max(maxArea, area);
+        if(height[left] < height[right]){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+    return maxArea;
+};
