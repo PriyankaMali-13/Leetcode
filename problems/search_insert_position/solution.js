4203a0f72234bd292480c9725a26fc4f5b069d2c
Priyanka Mali <priyankamali0000@gmail.com>
2026-07-14 15:07:08 +0000

    Sync LeetCode submission - Search Insert Position (javascript)

--- problems/search_insert_position/solution.js
+++ problems/search_insert_position/solution.js
@@ -3,13 +3,14 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function (nums, target) {
-    let low = 0, high = nums.length - 1;
-    while (high >= low) {
-        let mid = low + Math.floor((high - low)/2);
-        if (nums[mid] == target) return mid;
-        if (target < nums[mid]) high = mid - 1;
-        else low = mid + 1;
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length-1;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(target === nums[mid]) return mid;
+        else if(target<nums[mid]) right = mid-1;
+        else left = mid+1;
     }
-    return low;
+    return left;
 };
