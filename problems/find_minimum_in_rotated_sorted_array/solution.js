271810e23880a5279c0df3a40633aaa8eb5ed0be
Priyanka Mali <priyankamali0000@gmail.com>
2026-07-16 18:08:12 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array (javascript)

--- problems/find_minimum_in_rotated_sorted_array/solution.js
+++ problems/find_minimum_in_rotated_sorted_array/solution.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    return Math.min(...nums);
+    let left = 0;
+    let right = nums.length -1;
+    while(left < right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] > nums[right]){
+            left = mid+1;
+        }
+        else{
+            right = mid;
+        }
+    }
+    return nums[left];
 };
