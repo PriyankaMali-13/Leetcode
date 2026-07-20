f0f774195af1d278db5e1e2dd3491f0e44ff3f1c
Priyanka Mali <priyankamali0000@gmail.com>
2026-07-20 14:49:09 +0000

    Sync LeetCode submission - Find the Index of the First Occurrence in a String (javascript)

--- problems/find_the_index_of_the_first_occurrence_in_a_string/solution.js
+++ problems/find_the_index_of_the_first_occurrence_in_a_string/solution.js
@@ -3,12 +3,12 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function (haystack, needle) {
-    if (needle.length > haystack.length) return -1;
-    for (let i = 0; i <= haystack.length - needle.length; i++) {
-        if (haystack.slice(i, i + needle.length) == needle) {
-            return i;
-        }    
+var strStr = function(haystack, needle) {
+    for(let i=0; i<haystack.length; i++){
+        for(let j=0; j<needle.length; j++){
+            if(haystack[i+j] != needle[j]) break;
+            if(j == needle.length-1) return i;
+        }
     }
     return -1;
 };
