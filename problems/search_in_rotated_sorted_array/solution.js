26aba88733755dee8d1993d55ceed722293e856f
Priyanka Mali <priyankamali0000@gmail.com>
2026-07-25 15:47:36 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (javascript)

--- problems/search_in_rotated_sorted_array/solution.js
+++ problems/search_in_rotated_sorted_array/solution.js
@@ -4,9 +4,26 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] === target){
-            return i;
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        
+        if(nums[mid] === target) return mid;
+        
+        if(nums[left] <= nums[mid]){  //left half sorted
+            if(nums[left] <= target && target < nums[mid]){
+                right = mid-1;   
+            } else {
+                left = mid+1;    
+            }
+        } else {  // right half sorted
+            if(nums[mid] < target && target <= nums[right]){
+                left = mid+1;    
+            } else {
+                right = mid-1;   
+            }
         }
     }
     return -1;
