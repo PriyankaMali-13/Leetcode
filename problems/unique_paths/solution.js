f212eed6a850c078e4703b971bf71a24278257c5
Priyanka Mali <priyankamali0000@gmail.com>
2026-07-29 18:58:02 +0000

    Sync LeetCode submission - Unique Paths (javascript)

---
+++ problems/unique_paths/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    //create a 2d arr
+    let d = Array.from({length:m}, ()=> new Array(n).fill(1));
+    for(let i=1; i<m; i++){
+        for(let j=1; j<n; j++){
+            d[i][j] = d[i-1][j] + d[i][j-1];
+        }
+    }
+    return d[m-1][n-1];
+};
