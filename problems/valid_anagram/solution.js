5b3201ad619f3fc0409a3aa45d7f691b4e3c9489
Priyanka Mali <priyankamali0000@gmail.com>
2026-08-30 18:35:16 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

--- problems/valid_anagram/solution.js
+++ problems/valid_anagram/solution.js
@@ -4,31 +4,51 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length !== t.length){
-        return false
-    }
-    let dict = {};
+    if(s.length !== t.length) return false;
     
-    for(let char of s){
-        if(char in dict){
-            dict[char]++;
+    let map = {};
+    
+    for(let char of s) map[char] = (map[char] || 0) + 1;  // increment for s
+    for(let char of t) map[char] = (map[char] || 0) - 1;  // decrement for t
+    
+    for(let val of Object.values(map)){
+        if(val !== 0) return false;  // any non-zero = not anagram
+    }
+    return true;
+};
+
+/* my approach
+var isAnagram = function(s, t) {
+
+    if(s.length !== t.length) return false;
+    
+    let dict_s = {};
+    let dict_t = {};
+
+    for(let i of s){
+        if(i in dict_s){
+            dict_s[i] += 1;
         }
         else{
-            dict[char] = 1;
+            dict_s[i] = 1;
         }
     }
 
-    for(let char of t){
-        if(!(char in dict)){
-            return false;
+    for(let j of t){
+        if(j in dict_t){
+            dict_t[j] += 1;
         }
-        dict[char]--;
-        if(dict[char] < 0){
-            return false;
+        else{
+            dict_t[j] = 1;
         }
     }
-
-    return true;
-
     
+
+    for(let [key, val] of Object.entries(dict_s)){
+        if(dict_t[key] !== val) return false;
+    }
+    return true;
 };
+
+console.log(isAnagram("anagram", "nagaram"));
+ */
