425fff138e368cc660b2122709572d6c97f23380
Priyanka Mali <priyankamali0000@gmail.com>
2026-09-02 13:48:12 +0000

    Sync LeetCode submission - Longest Common Prefix (javascript)

--- problems/longest_common_prefix/solution.js
+++ problems/longest_common_prefix/solution.js
@@ -3,13 +3,34 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return "";
     let prefix = strs[0];
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.slice(0, -1);
-            if (!prefix) return "";
+    for(let i=1; i<strs.length; i++){
+        let k = 0;
+        while(k < prefix.length && k < strs[i].length){
+            //strs[i][k] cmp with curr str of chars
+            if(prefix[k] === strs[i][k]){
+                k++;
+            }
+            else{
+                break;
+            }
+        }
+        prefix = prefix.slice(0, k);
+    }
+
+    return prefix;
+
+};
+
+/**
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0];
+    for(let i = 1; i < strs.length; i++){
+        while(!strs[i].startsWith(prefix)){
+            prefix = prefix.slice(0, prefix.length - 1);  // shrink by 1
+            if(prefix === "") return "";
         }
     }
     return prefix;
 };
+ */
